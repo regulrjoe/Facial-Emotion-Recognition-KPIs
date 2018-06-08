8d9fc8c1b5963dd96d53086dcdff709cd4c3e69b
Antonio <tjmp94@gmail.com>
2018-06-07 20:36:40 -0500

    Connect functions

--- julia/src/kpi_age_gender.jl
+++ julia/src/kpi_age_gender.jl
@@ -2,17 +2,22 @@ module KPIAgeGender
 
 using DataFrames, StatPlots
 
-include("src/data_access.jl")
-
 plotly() # Plotting Front-end
 
 function run_kpi(data::DataFrames.DataFrame)
+    cleanData = structure_data(data)
+    plot_data(cleanData)
 end
 
-function structure_data()
+function structure_data(data::DataFrames.DataFrame)
+    sum_happiness = 0
+    for item in data[:Age]
+        sum_happiness += item
+    return data
 end
 
-function plot_data()
+function plot_data(data::DataFrames.DataFrame)
+
 end
 
 end
